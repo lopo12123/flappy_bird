8cc71c722943458e7a2d11296d9f5f962e7fb044
lopo <lopo@zju.edu.cn>
2024-08-14 23:38:15 +0800

    feat: marvelous!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use bevy::prelude::{App, Commands, Component, IntoSystemConfigs, Query, Startup, Update, With};
+use bevy::DefaultPlugins;
+use bevy::prelude::{App, Commands, Component, IntoSystemConfigs, Plugin, Query, Res, ResMut, Resource, Startup, Time, Timer, TimerMode, Update, With};
 
 #[derive(Component)]
 struct Person;
@@ -11,19 +12,30 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Stuart".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello, {}!", name.0);
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello, {}!", name.0);
+        }
     }
 }
 
-fn hello_world() {
-    println!("hello world!");
+struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, greet_people);
+    }
 }
 
 fn main() {
     App::new()
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, greet_people))
+        .add_plugins(DefaultPlugins)
+        .add_plugins(HelloPlugin)
         .run();
 }
